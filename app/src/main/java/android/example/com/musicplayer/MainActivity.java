d9fc1ecb1adc117e19bc8986cce86ad80a1794b0
Ravi Kumar <ravikr42@gmail.com>
2018-02-05 22:27:04 +0530

    Added Raw Song file and modified activity_main.xml

--- app/src/main/java/android/example/com/musicplayer/MainActivity.java
+++ app/src/main/java/android/example/com/musicplayer/MainActivity.java
@@ -1,7 +1,9 @@
 package android.example.com.musicplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.banjarani);
+
+
+
+
+    public void play(View view){
+        mediaPlayer.start();
+    }
+
+    public void pause(View view){
+        mediaPlayer.pause();
     }
 }
